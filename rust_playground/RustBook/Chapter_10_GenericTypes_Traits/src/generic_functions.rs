93b3abd95015549c23d000c63353f117b4779a6c
romaqQ <schick.roman@outlook.com>
2022-07-12 23:36:37 +0200

    Chapter 10_Part1

---
+++ rust_playground/RustBook/Chapter_10_GenericTypes_Traits/src/generic_functions.rs
@@ -0,0 +1,51 @@
+pub fn run(){
+    println!("Hello world of Rust Generics");
+
+    // Extracting Functions
+    let number_list = vec![34,50,25,100,65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+    let number_list = vec![34,50,25,100,65, 700];
+    let largest:i32 = largest_number(number_list);
+
+    println!("The largest number is {}", largest);
+
+    // Working with Generic Types and Functions
+    let char_list = vec!['y', 'm', 'z', 't'];
+
+    let largest:char = get_largest_gen(char_list);
+    
+    println!("The largest element is {}", largest);
+    let number_list = vec![34,50,25,100,65, 700, 999];
+    let largest:i32 = get_largest_gen(number_list);
+    println!("The largest element is {}", largest);
+}
+// Defining a function that can be called within main to perform same action as inline code and DRY
+fn largest_number(number_list: Vec<i32>) -> i32 {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+// Same function as above, but now with generic types
+// Generic types are defined in the <> brakcets <T> stands for <Type> 
+fn get_largest_gen<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
--- rust_playground/RustBook/Chapter_10_GenericTypes_Traits/src/main.rs
+++ rust_playground/RustBook/Chapter_10_GenericTypes_Traits/src/main.rs
@@ -1,3 +1,8 @@
+mod generic_functions;
+mod generic_structs;
+
 fn main() {
     println!("Hello, world!");
+    generic_functions::run();
+    generic_structs::run();
 }
