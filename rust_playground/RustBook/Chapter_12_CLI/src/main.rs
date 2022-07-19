f3e9aa002f6a525066adcdec9f1243f6c849a6e2
romaqQ <schick.roman@gmx.net>
2022-07-19 13:22:23 +0000

    Chapter_12 Part II

--- rust_playground/RustBook/Chapter_12_CLI/src/main.rs
+++ rust_playground/RustBook/Chapter_12_CLI/src/main.rs
@@ -1,54 +1,34 @@
 use std::env;
-use std::fs; // filesystem
 use std::process;
-use std::error::Error;
+
+use Chapter_12_CLI::Config; //name of the library crate (where the lib.rs is stored)
+
+
+// Redirecting Output of command line output to file
+// cargo run > output.txt
+// for this we need the eprintln! macro
 
 fn main() {
     // this command collects the arguments thar are being passed to cargo run 
     let args: Vec<String> = env::args().collect();
     // cargo run test sample.txt
+
+    // Constructing our Config struct
     let config = Config::new(&args).unwrap_or_else(|err| {
         // This following closure will be executed in the Error case of the Result type
         // the Ok case will just return the result of the function
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1); // this will terminate the program with the status code 1
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-    println!("{:?}", args);
+    eprintln!("Searching for {}", config.query);
+    eprintln!("In file {}", config.filename);
+    eprintln!("{:?}", args);
 
-    if let Err(e) = run(config) { // if the code run(config) results in an error, then run the code inside the if block
-        println!("Application error: {}", e);
+    if let Err(e) = Chapter_12_CLI::run(config) { // if the code run(config) results in an error, then run the code inside the if block
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
-}
-
-fn run(config:Config) -> Result<(), Box<dyn Error>> { // dyn Error means return any type of error
-    // the ? takes care of the Error
-    let contents: String = fs::read_to_string(config.filename)?; // the question mark will return the error in case the function call fails
-        //.expect("Something went wrong reading the file"); // since read_to_string returns either a Ok(String) or an Error; if the Error is returned then we catch it with .expect
-    
-    println!("With text: \n{}", contents);
-    Ok(())
-}
-
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query: String = args[1].clone(); // in order to avoid passing the ownership
-        let filename: String = args[2].clone();
-        Ok(Config{query, filename})
-    }
 }
 
