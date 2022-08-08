d00a4b54537a1119e9994640985e91aae753cbe7
romaqQ <schick.roman@gmx.net>
2022-08-08 19:22:00 +0000

    Chapter 19: Part II

--- rust_playground/RustBook/Chapter_19_AdvancedRust/src/main.rs
+++ rust_playground/RustBook/Chapter_19_AdvancedRust/src/main.rs
@@ -1,7 +1,11 @@
 // ToDo: Writing Unsafe Rust -> check chapter 19 book / youtube
 
 mod advanced_traits;
+mod advanced_types;
+mod advanced_functions_and_closures;
 fn main() {
     println!("Hello to Advanced Rust!");
     advanced_traits::run();
+    advanced_types::run();
+    advanced_functions_and_closures::run();
 }
