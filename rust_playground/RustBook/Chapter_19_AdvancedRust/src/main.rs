806393505e82d9768b1ba6bf5ef0f27269163f69
romaqQ <schick.roman@gmx.net>
2022-08-10 11:41:17 +0000

    Chapter 19: Final Part

--- rust_playground/RustBook/Chapter_19_AdvancedRust/src/main.rs
+++ rust_playground/RustBook/Chapter_19_AdvancedRust/src/main.rs
@@ -4,10 +4,12 @@ mod advanced_traits;
 mod advanced_types;
 mod advanced_functions_and_closures;
 mod declarative_macros;
+mod procedural_macros;
 fn main() {
     println!("Hello to Advanced Rust!");
     advanced_traits::run();
     advanced_types::run();
     advanced_functions_and_closures::run();
     declarative_macros::run();
+    procedural_macros::run();
 }
